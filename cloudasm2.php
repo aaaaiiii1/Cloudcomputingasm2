997dbe164d39a77171cc8c8b3c956f2638f79cf3
Nguyen Quang Vinh <vinh06031990day@gmail.com>
2019-09-06 16:31:24 +0700

    cloudasm2.php created
    new php for asm2

---
+++ cloudasm2.php
@@ -0,0 +1,11 @@
+<!--
+To change this template file, choose Tools | Templates
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    <body>
+        <?php
+         echo 'this is for asm2 cloud computing';
+</html>
